b47deb71e2f46a39fb29a8db01e18416938f12e9
StanderSam <samuel6304@gmail.com>
2020-07-10 03:35:22 -0400

    Made dataloader

--- Model/BaseLine.py
+++ Model/BaseLine.py
@@ -5,6 +5,7 @@ import random
 import torch.utils.data
 import time
 import os
+import io
 import torchvision
 from torchvision import datasets, models, transforms
 import matplotlib.pyplot as plt
@@ -12,79 +13,74 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from torch.utils.data import Dataset
 import matplotlib.pyplot as plt
 import pandas as pd
 import cv2
 from PIL import Image
 from torchvision.transforms import ToTensor, ToPILImage, Normalize, Compose
 from torch.utils.data import DataLoader
-import numpy as np
-import random
-
-import io
-from torch.utils.data import Dataset
-import torch
 
 # Reference: https://stackoverflow.com/questions/56696147/pytorch-how-to-create-a-custom-dataset-with-reference-table
 
-class CustomDatasetFromImages(Dataset):
+class Dataloader(Dataset):
     def __init__(self, csv_path, transform = None, test = False):
         """
         Args:
             csv_path (string): path to csv file
-            img_path (string): path to the folder where images are
-            transform: pytorch transforms for transforms and tensor conversion
+            transform: pytorch transforms for transform
+            test: whether to generate train/test loader
+
         """
-        # Transforms
-        self.to_tensor = transforms.ToTensor()
-
+        
         # Read the csv file
-        self.data_info = pd.read_csv(csv_path)
+        pf = pd.read_csv(csv_path)
 
+        # Load train/test data
         self.test = test
 
         if self.test == True:
-            self.data_info = self.data_info[self.data_info.iloc[:, 3] == 0]
+            self.data_info = pf[pf.iloc[:, 3] == 0]
         else:
-            self.data_info = self.data_info[self.data_info.iloc[:, 3] == 1]
+            self.data_info = pf[pf.iloc[:, 3] == 1]
+
         # First column contains the image paths
-        self.image_arr = np.asarray(self.data_info.iloc[:, 1])
+        self.paths = np.asarray(self.data_info.iloc[:, 1])
 
         # Second column is the labels
-        self.label_arr = np.asarray(self.data_info.iloc[:, 2])
+        self.labels = np.asarray(self.data_info.iloc[:, 2])
 
-        # Third column is for an operation indicator
-        self.operation_arr = np.asarray(self.data_info.iloc[:, 3])
+        # Third column is for an train Boolean
+        self.trainBools = np.asarray(self.data_info.iloc[:, 3])
 
         # Calculate len
         self.data_len = len(self.data_info.index)
 
-        
-
+        # Transform function
         self.transform = transform
+
+        # Transform to tensor
+        self.to_tensor = transforms.ToTensor()
 
     def __getitem__(self, index):
 
         # Get image name from the pandas df
-        single_image_name = self.image_arr[index]
+        imageName = self.paths[index]
         dirname = os.path.dirname(__file__)
-        image_path = os.path.join(dirname, '..//Picture//2017-IWT4S-CarsReId_LP-dataset', single_image_name)
+        image_path = os.path.join(dirname, '..//Picture//2017-IWT4S-CarsReId_LP-dataset', imageName)
 
         # Open image
-        img_as_img = Image.open(image_path)
-
-        # Check if there is an operation
-        some_operation = self.operation_arr[index]
-
+        img = Image.open(image_path)
+        
         # Transform image to tensor
         if self.transform !=None:
-            img_as_img = self.transform(img_as_img)
-        img_as_tensor = self.to_tensor(img_as_img)
+            img = self.transform(img)
+        imgTensor = self.to_tensor(img)
+        
+        # Get license plate number
+        label = self.labels[index]
 
-        # Get label(class) of the image based on the cropped pandas column
-        single_image_label = self.label_arr[index]
-
-        return (img_as_tensor, single_image_label)
+        return (imgTensor, label)
 
     def __len__(self):
         return self.data_len
@@ -100,7 +96,7 @@ if __name__ == '__main__':
     
     # train_data = datasets.ImageFolder(train_dir, transform=data_transform)
     
-    train_data = CustomDatasetFromImages(filename, transform=data_transform, test = False)
+    train_data = Dataloader(filename, transform=data_transform, test = False)
 
     print('Num training images: ', len(train_data))
     batch_size = 128
