7215800c58cb7e3b9b8e6faa072266cfab50cc93
Saury <48082962+SauryCC@users.noreply.github.com>
2020-07-19 01:21:36 +0800

    ver 3

--- Image process/opencv/plateToCharactersModule.py
+++ Image process/opencv/plateToCharactersModule.py
@@ -1,7 +1,11 @@
 import cv2
 import numpy as np
 print("cv2 imported")
+import pytesseract
 
+licenseGuess = ''
+
+pytesseract.pytesseract.tesseract_cmd = 'C:\Program Files (x86)\Tesseract-OCR\\tesseract.exe'
 # ------------------detect shape----------------------------------------
 
 
@@ -18,7 +22,7 @@ def cropout(img, imgcpy):
         # to cutoff useless contours, test etc
         # try it out to find area limit
         if area > 100:
-            #print(area)
+            print(area)
             #cv2.drawContours(imgcpy, cnt, -1, (255, 0, 0), 1)
             # find arc length, (target, if  target is enclosed)
             para = cv2.arcLength(cnt, True)
@@ -36,6 +40,55 @@ def cropout(img, imgcpy):
             imggCropped = imgcpy[y:y+h, x:x+w]
             return imggCropped
 
+def furtherCut(img, myColors):
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    for color in myColors:
+        lower = np.array(color[0:3])
+        upper = np.array(color[3:6])
+        mask = cv2.inRange(imgHSV, lower, upper)
+    # cut off horizontal bias
+    count = 10
+    for i in range(1,10):
+        binaryImg = mask[:,(i-1)*5:i*5]
+        height, width = binaryImg.shape[:2]
+        print("a-------------------", height, width)
+        whitePercent = cv2.countNonZero(binaryImg)/(height*width)
+        if whitePercent> 0.7:
+            count = i
+            #print('percentage:', whitePercent)
+            break
+    #img = img[:, count * 5:]
+
+    # cut off vertical bias
+    countvertop = 10
+    for i in range(1, 10):
+        binaryImg = mask[(i - 1) * 5:i * 5, :]
+        height, width = binaryImg.shape[:2]
+        whitePercent = cv2.countNonZero(binaryImg) / (height * width)
+        if whitePercent > 0.5:
+            countvertop = i
+            print('percentage:', whitePercent)
+            break
+    # cut top and left
+    img = img[countvertop*5:,count*5:]
+
+    countverbottom = 10
+    for i in range(1, 10):
+        height, width = img.shape[:2]
+        binaryImg = mask[height-(i * 5): height- ((i - 1) * 5), :]
+
+        whitePercent = cv2.countNonZero(binaryImg) / (height * width)
+        if whitePercent < 0.5:
+            countverbottom = i
+            print('percentage:', whitePercent)
+            break
+
+    img = img[:(height - countverbottom*5), :]
+
+
+    return img
+
+
 
 
 def findColor(img, myColors, penColors, imgContour, enhanced, myPoints):
@@ -52,7 +105,34 @@ def findColor(img, myColors, penColors, imgContour, enhanced, myPoints):
         # draw a line with center top of color contour
         centerx, centery = getContour(mask1, imgContour, enhanced, myPoints)
         cv2.imshow('Cropped out on stretched picture', enhanced)
-    return mask1
+
+        licenseGuess =pytesseract.image_to_string(mask)
+        print(licenseGuess)
+
+
+
+        # ----------------------------------------------------------
+        pytesseract.pytesseract.tesseract_cmd = 'C:\Program Files (x86)\Tesseract-OCR\\tesseract.exe'
+
+        imggg = mask.copy()
+
+        imggg = cv2.cvtColor(imggg, cv2.COLOR_BGR2RGB)
+
+        hImg, wImg, _ = imggg.shape
+        boxes = pytesseract.image_to_boxes(imggg)
+        for b in boxes.splitlines():
+            print(b)
+            b = b.split(' ')
+            print(b)
+            x, y, w, h = int(b[1]), int(b[2]), int(b[3]), int(b[4])
+            cv2.rectangle(imggg, (x, hImg - y), (w, hImg - h), (50, 50, 255), 2)
+            cv2.putText(imggg, b[0], (x, hImg - y + 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (50, 50, 255), 2)
+
+        cv2.imshow('imggg', imggg)
+
+        # ----------------------------------------------------------------------------------------
+
+    return mask1, licenseGuess
 
 
 # -------------------------------  find contour for the color tracked --------------------------
@@ -72,7 +152,7 @@ def getContour(img, imgContour, enhanced, myPoints):
         # to cutoff useless contours, test etc
         # try it out to find area limit
         print(area)
-        if 1000 < area < 7000:
+        if 1000 < area < 9000:
             cv2.drawContours(imgContour, cnt, -1, (255, 0, 0), 10)
             # find arc length, (target, if  target is enclosed)
             para = cv2.arcLength(cnt, True)
@@ -83,8 +163,10 @@ def getContour(img, imgContour, enhanced, myPoints):
             objContour = len(approx)
             # draw a box around the shape
             x, y, w, h = cv2.boundingRect(approx)
-            cv2.rectangle(enhanced, (x, y), (x+w, y+h), (0, 255, 0), 3)
-            myPoints.append([x,y,w,h])
+            # if a bounding box has width > height, it must be not a letter
+            if w < h:
+                cv2.rectangle(enhanced, (x, y), (x+w, y+h), (0, 255, 0), 3)
+                myPoints.append([x,y,w,h])
     return x+w//2, y
 
 
@@ -125,17 +207,34 @@ def slicePic(img):
     # copy original image to draw onto it
     imgcpy = img.copy()
     # crop img out
-    imggCropped = cropout(imgCanny,imgcpy )
+
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    for color in myColors:
+        lower = np.array(color[0:3])
+        upper = np.array(color[3:6])
+        mask = cv2.inRange(imgHSV, lower, upper)
+
+    imggCropped = cropout(mask, imgcpy)
+
+    if imggCropped is None:
+        imggCropped = imgcpy.copy()
     enhanced = cv2.resize(imggCropped,(600,200))
+    enhanced= furtherCut(enhanced, myColors)
+    enhanced = cv2.resize(enhanced, (600, 200))
+    cv2.imshow('further enhanced', enhanced)
+
+
+
+
     imgContour = enhanced.copy()
-    findColor(imgContour, myColors, penColors, imgContour, enhanced, myPoints)
+    mask1, licenseGuess = findColor(imgContour, myColors, penColors, imgContour, enhanced, myPoints)
     print(myPoints)
     #cv2.imwrite( "imgs/test.png", enhanced )
     imgSrc = enhanced.copy()
     outputs = getIndividualPic(imgSrc, myPoints, myColors)
     for i in range(len(outputs)):
         outputs[i] = cv2.resize(outputs[i] ,(280,280))
-    return outputs
+    return outputs, licenseGuess
 
 
 # --------------------------------------- main block ------------------------------------------------
@@ -155,15 +254,22 @@ In some cases there will be missing numbers or characters, even random slices wi
 
 if __name__ == "__main__":
     # change this input image
-    img = cv2.imread("imgs/1.png")
+    img = cv2.imread("imgs/10.png")
     # slice numbers and characters out of the image
-    outputs = slicePic(img)
+    outputs, licenseGuess = slicePic(img)
+    licenseGuess = licenseGuess.replace(" ", "")
+    # prevent head over reading
+    if len(licenseGuess)>7:
+        licenseGuess = licenseGuess[-7:]
     # print out images
     imageName = 0
     for image in outputs:
         cv2.imshow(str(imageName), image)
+        # cv2.imwrite( "imgs/tests", enhanced )
         imageName += 1
+
     # print original image
     cv2.imshow('original image', img)
+    print(licenseGuess)
     # keep pictures displayed
     cv2.waitKey(0)
